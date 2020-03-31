1ec31c81f7f9ef19bb61c37c5167436a02988340
zembrani <renato.zembrani@hotmail.com>
2020-03-31 15:42:54 -0300

    Style ready

---
+++ Windows-Calculator/javascript.js
@@ -0,0 +1,12 @@
+function changeVisor() {
+    //document.getElementsByClassName("numbers").onclick
+    console.log("hi");
+}
+
+
+function initMyEvents() {
+	changeVisor();
+	// Any other events you use can be added here in the same form.
+}
+
+window.onload = initMyEvents;
