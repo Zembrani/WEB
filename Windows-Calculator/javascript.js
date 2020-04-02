8f0727b5a11129d6cfa13f440f3fd6c4420a89a1
zembrani <renato.zembrani@hotmail.com>
2020-04-02 09:41:17 -0300

    Implementing screen

--- Windows-Calculator/javascript.js
+++ Windows-Calculator/javascript.js
@@ -1,12 +1,63 @@
-function changeVisor() {
-    //document.getElementsByClassName("numbers").onclick
-    console.log("hi");
+
+
+window.onload = function () {
+    click();
+};
+
+function click () {
+    var button = document.querySelectorAll("#bottom span");
+    var expression = document.getElementById("expression");
+    var res = document.getElementById("res");
+    
+    
+    for (let i = 0; i < button.length; i++) {
+        var keyboard = button[i];
+        keyboard.onclick = function () {
+            var number = this.dataset["number"];
+            screen(number);
+        }
+    }
 }
 
-
-function initMyEvents() {
-	changeVisor();
-	// Any other events you use can be added here in the same form.
+function clean(number) {
+    if(number === "C" || number === "CE") {
+        res.innerHTML = "0";
+            if(number === "C") {
+                expression.innerHTML = "&nbsp";
+            }
+    }
 }
 
-window.onload = initMyEvents;
+function addNumber(number, resVal,expreVal, symbol) {
+    if(parseInt(number)>-1 && parseInt(number)<10){
+        var lastChar = expreVal.substring(expreVal.length-1, expreVal.length);
+        if(res.innerHTML==="0" || lastChar in symbol){
+            res.innerHTML = number;       
+        } else {          
+            res.innerHTML = resVal + number;
+        }
+    }
+}
+
+function addExpression(number, expreVal, resVal, symbol) {
+    if(number in symbol) {
+        if(expreVal==="&nbsp"){
+            alert(resVal);
+            expression.innerHTML = resVal + " " + symbol[number];        
+        }else{
+            expression.innerHTML = expreVal + " " + resVal + " " + symbol[number];
+        }
+    }
+}
+
+function screen(number) {
+    var expreVal = expression.innerHTML;
+    var resVal = res.innerHTML;  
+    var symbol = {"+":"+", "-":"-", "*":"*", "/":"/"};
+
+    clean(number);
+    addNumber(number, resVal, expreVal, symbol);
+    addExpression(number, expreVal, resVal, symbol);
+        
+
+}
